812b9e042b25e92d913d224cc8c37d21da2d4642
SeniorDevCode <senior.dev.software@gmail.com>
2018-04-26 13:37:59 +0900

    Update contributors

--- lib/shopify/resources/metafield.ex
+++ lib/shopify/resources/metafield.ex
@@ -3,23 +3,6 @@ defmodule Shopify.Metafield do
 
   @derive [Poison.Encoder]
 
-  @singular "metafield"
-  @plural "metafields"
-
-  use Shopify.Resource,
-    import: [
-      :find,
-      :all,
-      :count,
-      :create,
-      :update,
-      :delete
-    ]
-
-  alias Shopify.{
-    Metafield
-  }
-
   defstruct [
     :created_at,
     :description,
@@ -32,51 +15,4 @@ defmodule Shopify.Metafield do
     :value_type,
     :updated_at
   ]
-
-  @doc false
-  def empty_resource do
-    %Metafield{
-      key: "",
-      value: "",
-      value_type: "string",
-      namespace: ""
-    }
-  end
-
-  ~w(blogs collections customers draft_orders orders pages products)
-  |> Enum.each(fn (name) ->
-    def unquote(:"#{name}")(session, parent_id, params \\ %{}) do
-      session
-      |> Request.new(metafield_url(unquote(name), parent_id), params, plural_resource())
-      |> Client.get()
-    end
-  end)
-
-  [["blogs", "articles"], ["products", "variants"]]
-  |> Enum.each(fn ([parent_name, child_name]) ->
-    def unquote(:"#{child_name}")(session, parent_id, child_id, params \\ %{}) do
-      session
-      |> Request.new(metafield_url(unquote(parent_name), parent_id,unquote(child_name), child_id), params, plural_resource())
-      |> Client.get()
-    end
-  end)
-
-  @doc false
-  def find_url(id), do: @plural <> "/#{id}.json"
-
-  @doc false
-  def all_url, do: @plural <> ".json"
-
-  @doc false
-  def count_url, do: @plural <> "/count.json"
-
-  @doc false
-  def metafield_url(parent_name, parent_id) do
-    parent_name <> "/#{parent_id}/" <> @plural <> ".json"
-  end
-
-  @doc false
-  def metafield_url(parent_name, parent_id, child_name, child_id) do
-    parent_name <> "/#{parent_id}/" <> child_name <> "/#{child_id}/" <> @plural <> ".json"
-  end
 end
